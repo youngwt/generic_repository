b51112c0bdcc77fbacdd82c97c631faa6166cc90
Wayne Atkinsin-Young <youngwt@gmail.com>
2021-03-14 21:07:55 +0000

    initial commit
    fresh solution

---
+++ GenericRepositoryTests/GenericRepositoryFixture.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Generic_Repository
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
